c8b31bd74d73de1f14c1b0269fa2a754e86de5e0
kevinsshaffer <kevinsshaffer@gmail.com>
2019-03-20 21:22:05 -0600

    Initial Commit

--- EncryptPasswordWithCsp/Program.cs
+++ EncryptPasswordWithCsp/Program.cs
@@ -1,15 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace EncryptPasswordWithCsp
 {
     class Program
     {
+        static string filePath = Path.Combine(Directory.GetCurrentDirectory(), "config.txt");
+        const string CONTAINER_NAME = "HamRadioApplication";
+        static CspParameters CspParameters => new CspParameters(1)
+        {
+            KeyContainerName = CONTAINER_NAME,
+            Flags = CspProviderFlags.UseMachineKeyStore,
+            ProviderName = "Microsoft Strong Cryptographic Provider"
+        };
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Type in your password");
+
+            string password = Console.ReadLine();
+
+            Console.Clear();
+
+            byte[] encodedPassword = Encoding.UTF8.GetBytes(password);
+            byte[] encryptedPassword = EncrypyData(encodedPassword);
+            string base64EncryptedPassword = Convert.ToBase64String(encryptedPassword);
+
+            File.WriteAllText(filePath, base64EncryptedPassword);
+
+            Console.WriteLine("Encrypted password written to file as a base 64 string.");
+            Console.ReadKey();
+
+            base64EncryptedPassword = File.ReadAllText(filePath);
+            byte[] decryptedPassword = DecryptData(Convert.FromBase64String(base64EncryptedPassword));
+            string decodedPassword = Encoding.UTF8.GetString(decryptedPassword);
+
+            Console.WriteLine($"Your password is {decodedPassword}");
+
+            Console.ReadKey();
+        }
+
+        static void DeleteKeyContainerFromMachineKeyStore()
+        {
+            using (var rsa = new RSACryptoServiceProvider(CspParameters) { PersistKeyInCsp = false })
+                rsa.Clear();
+        }
+
+        static byte[] EncrypyData(byte[] data)
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048, CspParameters) { PersistKeyInCsp = true })
+                return rsa.Encrypt(data, false);
+        }
+
+        static byte[] DecryptData(byte[] data)
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048, CspParameters))
+                return rsa.Decrypt(data, false);
         }
     }
 }
